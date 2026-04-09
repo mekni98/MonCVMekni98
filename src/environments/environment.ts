8555faf7e3c3c28e446c3ba0f3e238bd4e663804
mekni98 <meknimed01@gmail.com>
2026-04-09 20:15:38 +0100

    eenBVVVVVEEaatt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   emailjs: {
-    serviceID: 'DUMMY',
-    templateID: 'DUMMY',
-    publicKey: 'DUMMY'
+    serviceID: 'service_4ix05me',
+    templateID: 'template_ykcj0kp',
+    publicKey: 'EvtQ-jAmBFP-GEmNc'
   }
 };
