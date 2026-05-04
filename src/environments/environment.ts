2bec4250c5917dc5a9d9e66cb2c7e6b9474bcabd
mekni98 <139440897+mekni98@users.noreply.github.com>
2026-05-04 07:57:24 +0100

    Replace EmailJS with placeholders
    Updated EmailJS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   emailjs: {
-    serviceID: 'service_4ix05me',
-    templateID: 'template_ykcj0kp',
-    publicKey: 'EvtQ-jAmBFP-GEmNc'
+    serviceID: 'XXXXXXXXXX',
+    templateID: 'XXXXXXXXXX',
+    publicKey: 'XXXXXXXXXXXXXX'
   }
 };
